a718294b030d41e45c8d6b9efe222bb7c3aedf18
aurpon70-gif <aurpon70@gmail.com>
2026-07-15 19:45:15 -0400

    Initial commit

---
+++ Lesson 2/understandingthevariables.py
@@ -0,0 +1,8 @@
+x=5
+y="Aurpon"
+print(x,y)
+codingal="codingal"
+print(codingal)
+# User input
+name = input("Enter your name: ")
+print("\nHello, ", name, "\nwelcome to codingal!")
---
+++ Lesson 2/welcometoworldofcoding.py
@@ -0,0 +1,8 @@
+print("Welcome to the world of programming!")
+print(4)
+# Next line after print
+print("hello Python\n")
+#Pass multiple argument for printing
+print("Hello",5)
+print("Welcome to", end="*")
+
