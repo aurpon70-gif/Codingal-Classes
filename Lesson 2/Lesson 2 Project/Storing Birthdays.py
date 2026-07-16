21a6916de815057d647a29eb5e6e7102c5993f1c
aurpon70-gif <aurpon70@gmail.com>
2026-07-15 20:48:43 -0400

    Describe your changes

---
+++ Lesson 2/Lesson 2 Project/Storing Birthdays.py
@@ -0,0 +1,15 @@
+# Create a dictionary to store birthdays
+birthdays = {
+    "Alice_Birthday": "Alice's birthday is May 14th",
+    "Bob_Birthday": "Bob's birthday is July 22th",
+    "Charlie_Birthday": "Charlie's birthday is October 5th",
+    "David_Birthday": "David's birthday is August 15th",
+    "Edward_Birthday": "Edward's birthday is February 8th"
+}
+
+# Print Alice's birthday
+print(birthdays["Alice_Birthday"])
+print(birthdays["Bob_Birthday"])
+print(birthdays["Charlie_Birthday"])
+print(birthdays["David_Birthday"])
+print(birthdays["Edward_Birthday"])
