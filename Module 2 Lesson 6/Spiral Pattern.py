b65e78f4b2ab734dddcfda03a57a50f54b313ce4
aurpon70-gif <aurpon70@gmail.com>
2026-09-13 19:30:01 -0400

    your message here

---
+++ Module 2 Lesson 6/Spiral Pattern.py
@@ -0,0 +1,12 @@
+import turtle
+my_wn = turtle.Screen()
+my_wn.bgcolor("light blue")
+my_wn.title("Turtle")
+my_pen = turtle.Turtle()
+size = 0
+while True:
+    for i in range(4):
+        my_pen.forward(size + 1)
+        my_pen.left(30)
+        size = size +4
+    size = size + 1
