9151afc9a3c6669033562ec8c624bd8f9db46d19
aurpon70-gif <aurpon70@gmail.com>
2026-09-20 11:32:24 -0400

    your message here

---
+++ Module 3 Lesson 1/Homework/Circumference.py
@@ -0,0 +1,6 @@
+def circumference(radius):
+    pi = 3.14
+    return 2 * pi * radius
+
+radius = float(input("Enter the radius of the circle: "))
+print("The circumference is approximately:", circumference(radius))
