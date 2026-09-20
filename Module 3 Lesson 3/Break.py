5ce906c19d3e268c8c435df49a7312de513d5c93
aurpon70-gif <aurpon70@gmail.com>
2026-09-20 19:39:40 -0400

    your message here

---
+++ Module 3 Lesson 3/Break.py
@@ -0,0 +1,11 @@
+# Take user input
+a = input ("Enter a word: ")
+# Program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1
+    #display result
+        print ("A has been found")
+        break #break statement
+    else:
+        #display result
+        print ("A not found")
---
+++ Module 3 Lesson 3/Examples/Example Return.py
@@ -0,0 +1,6 @@
+def func (a,b):
+    a = 7
+    return (a+b)
+
+sum = func(5,5)
+print (sum)
