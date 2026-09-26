d752dd8683ef618af69a28bbce0e05348d98dc33
aurpon70-gif <aurpon70@gmail.com>
2026-09-26 12:35:27 -0400

    your message here

--- Module 3 Lesson 5/Homework/Trigonometric Value.py
+++ Module 3 Lesson 5/Homework/Trigonometric Value.py
@@ -4,11 +4,11 @@ angle_degrees = float(input("Enter the angle in degrees: "))
 
 angle_radians = math.radians(angle_degrees)
 
-sine_val = math.sin(angle_radians)
-cosine_val = math.cos(angle_radians)
-tangent_val = math.tan(angle_radians)
+sin_val = math.sin(angle_radians)
+cos_val = math.cos(angle_radians)
+tan_val = math.tan(angle_radians)
 
 print(f"\nResults for {angle_degrees}°:")
-print(f"Sine: {sine_val}")
-print(f"Cosine: {cosine_val}")
-print(f"Tangent: {tangent_val}")
+print(f"Sin: {sin_val}")
+print(f"Cos: {cos_val}")
+print(f"Tan: {tan_val}")
